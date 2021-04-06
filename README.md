b4b35b19824219379bf97c60e01562e041030ab0
Saad Saif <saadgfx97@gmail.com>
2021-04-06 23:56:15 +0500

    added live link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+## [Live site](https://saad-saif-shopping-cart.surge.sh)
+
 React.FC is dicouraged
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import { store } from './store/store'
